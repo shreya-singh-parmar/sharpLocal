7ccf6fd08bd2d579c5c315b92300190eab53e6da
Shreya-Singh <shreyasingh14062000@gmail.com>
2023-09-13 10:26:08 +0530

    fifth commit

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const myForm = document.querySelector('#my_form');
 const nameInput = document.querySelector('#name');
 const emailInput = document.querySelector('#email');
+const numbInput = document.querySelector('#num');
 const msg= document.querySelector('.msg');
 const userlist = document.querySelector('#users');
  
@@ -8,14 +9,25 @@ myForm.addEventListener('submit',onSubmit);
 
   function onSubmit(e) {
     e.preventDefault();
-   if(nameInput.value === '' || emailInput.value === ''){
+   if(nameInput.value === '' || emailInput.value === '' || numbInput===''){
     msg.innerHTML = 'Please enter a feild';
     setTimeout(()=>msg.remove(),3000);
    }
    else{
-    localStorage.setItem('userName', nameInput.value);
-    localStorage.setItem('email', emailInput.value);
+    const li = document.createElement('li');
+    li.appendChild(document.createTextNode(`${nameInput.value}: ${emailInput.value}: ${numbInput.value}`));
+    userlist.append(li);
+     let myObj ={
+      name : nameInput.value,
+      email : emailInput.value,
+      number : numbInput.value
+
+    }
+    let myObj_ser = JSON.stringify(myObj);
+    localStorage.setItem(email.value, myObj_ser);
     nameInput.value='';
     emailInput.value='';
+    
+
    }
   }
