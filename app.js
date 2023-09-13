a0979b683dc7297346682ff60fcae9c0d63c38e5
Shreya-Singh <shreyasingh14062000@gmail.com>
2023-09-13 11:39:34 +0530

    sixth commit

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const msg= document.querySelector('.msg');
 const userlist = document.querySelector('#users');
  
 myForm.addEventListener('submit',onSubmit);
-
+userlist.addEventListener('click', onRemove);
   function onSubmit(e) {
     e.preventDefault();
    if(nameInput.value === '' || emailInput.value === '' || numbInput===''){
@@ -14,20 +14,43 @@ myForm.addEventListener('submit',onSubmit);
     setTimeout(()=>msg.remove(),3000);
    }
    else{
+    // Create a unique ID for the list item
+    const itemId = new Date().getTime();
+    //console.log(itemId);
     const li = document.createElement('li');
+    li.setAttribute('data-myObj_ser-id', itemId);
     li.appendChild(document.createTextNode(`${nameInput.value}: ${emailInput.value}: ${numbInput.value}`));
-    userlist.append(li);
-     let myObj ={
+    
+    var dlt = document.createElement('button');
+    dlt.className= 'dlt';
+     dlt.appendChild(document.createTextNode('delete'));
+     li.append(dlt);
+     userlist.append(li);
+          let myObj ={
       name : nameInput.value,
       email : emailInput.value,
       number : numbInput.value
 
     }
     let myObj_ser = JSON.stringify(myObj);
-    localStorage.setItem(email.value, myObj_ser);
+    localStorage.setItem(itemId.toString(), myObj_ser);
     nameInput.value='';
     emailInput.value='';
     
 
    }
+  }
+
+  function onRemove(e){
+    if(e.target.classList.contains('dlt')){
+      if(confirm("are u dlt?")){
+        var li = e.target.parentElement;
+        userlist.removeChild(li);
+        const itemId = li.getAttribute('data-myObj_ser-id');
+
+        // Remove the item from localStorage using the correct key
+        localStorage.removeItem(itemId); 
+        
+      }
+    }
   }
